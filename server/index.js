e2ad048f5982c9ff4e67b66747ecfd9bb7861420
Brian <blee0613@gmail.com>
2021-07-20 16:17:53 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+// const router = require('routes.js');
+
+// import db connection here
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/', router);
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(PORT, () => {
+
+});
