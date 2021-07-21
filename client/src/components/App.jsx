d467f16ad54f2ffad245008b36244698ae80514d
Brian <blee0613@gmail.com>
2021-07-21 12:31:38 -0700

    folder setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,11 +4,15 @@ import './App.css';
 function App() {
 
     const [products, setProducts] = useState([]);
+    const [currentProduct, setCurrentProduct] = useState(0)
+
+    // GET request to put all products into products state on Mount
 
 
     return (<div>
         <h1>Project Catwalk</h1>
         <h3>Components Here</h3>
+        {/* <Overview products={products} /> */}
     </div>)
 }
 
---
+++ client/src/components/related/Related.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+// import './App.css';
+
+function Related() {
+
+  const [outfit, setOutfit] = useState([]);
+
+    return (
+      <div className="related-products-section">
+        {/* Related Products Component */}
+        {/* Your Outfit Component */}
+      </div>
+    )
+}
+
+export default Related
