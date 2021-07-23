0ad82ef5348485e9c70da286351b24c315508227
Brian <blee0613@gmail.com>
2021-07-22 18:47:35 -0700

    added related items route

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,7 @@ const controller = require('./controllers');
 router.get('/products', controller.products.getProducts);
 router.get('/products/:product_id', controller.products.getProduct);
 router.get('/products/:product_id/styles', controller.products.getStyles);
+router.get('/products/:product_id/related', controller.products.getRelatedProducts);
 
 // router.get('/qa', controller.qa.get);
 
