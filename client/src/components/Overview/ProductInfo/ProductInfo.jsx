8cfa4267793019d4df6c8bff05305b0540f53e71
Corey Robinson <cr.contact90@gmail.com>
2021-07-24 12:06:44 -0700

    Removed product prop for now

--- client/src/components/Overview/ProductInfo/ProductInfo.jsx
+++ client/src/components/Overview/ProductInfo/ProductInfo.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function ProductInfo({product}) {
+// eslint-disable-next-line react/prop-types
+// eslint-disable-next-line no-unused-vars
 
+function ProductInfo() {
   return (
     <div>
       <h3>Product Info</h3>
