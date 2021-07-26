2826906284329e2aaffee9cdb2d3f1461f99f388
Corey Robinson <cr.contact90@gmail.com>
2021-07-26 09:53:53 -0700

    Built out skeleton for product info section for Top and Mid Component, testing gitignore dist folder

--- client/src/components/Overview/ProductInfo/ProductInfo.jsx
+++ client/src/components/Overview/ProductInfo/ProductInfo.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-
+import './ProductInfo.css';
+// eslint-disable-next-line import/extensions
+import Styles from './Styles.jsx';
 // eslint-disable-next-line react/prop-types
 // eslint-disable-next-line no-unused-vars
 
-function ProductInfo() {
+function ProductInfo({currentProduct, currentStyle}) {
+  const productStyles = currentStyle.results;
+
+//current style price
+// current SKU
+// current Image Array
+
   return (
-    <div>
-      <h3>Product Info</h3>
+    <div className="Product-InfoAll">
+      <div className="Product-Info-Top">
+        <h3>{currentProduct.category}</h3>
+        <h2>{currentProduct.name}</h2>
+        <span>{currentProduct.default_price}</span>
+      </div>
+      <div className="Product-Info-Mid">
+        <div className="Style-Title">
+          Style &gt;
+          {productStyles[0].name}
+        </div>
+        {productStyles.map((style, index) => (
+          <Styles style={style} key={productStyles[index].style_id} />
+        ))}
+      </div>
+      <div>Add Cart Section</div>
     </div>
   );
 }
