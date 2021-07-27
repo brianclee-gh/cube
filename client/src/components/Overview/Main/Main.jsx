0327ed398ac79d6fce53bcde07a17384d7c6f032
Corey Robinson <cr.contact90@gmail.com>
2021-07-27 15:24:22 -0700

    Testing Context

--- client/src/components/Overview/Main/Main.jsx
+++ client/src/components/Overview/Main/Main.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/extensions */
-import React, {useState} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { ProductsContext } from '../../state/ProductsContext.jsx';
 import Image from '../Image/Image.jsx';
 import ProductInfo from '../ProductInfo/ProductInfo.jsx';
 import Description from '../Description/Description.jsx';
@@ -8,16 +9,25 @@ import productStyleSampleData from '../productStyleSampleData.js';
 
 function Main() {
   // console.log(getProductSampleData);
-  const [current, setCurrent] = useState(getProductSampleData);
-  const [currentStyle, setStyle] = useState(productStyleSampleData);
-  const [currentImages, setCurrentImages] = useState(currentStyle.results);
+  const { currentProduct, currentStyle, getCurrentStyle, getCurrentProduct } = useContext(ProductsContext);
+  // const [current, setCurrent] = useState(getProductSampleData);
+  // const [currentStyle, setStyle] = useState(productStyleSampleData);
+  const [current, setCurrent] = useState();
+  const [style, setStyle] = useState();
+  // const [currentImages, setCurrentImages] = useState(currentStyle.results);
   // need to store style image array here, pass to image component,
   // pass a function down to update it based on style click
+  useEffect(() => {
+    getCurrentStyle('17067');
+    getCurrentProduct('17067');
+    // .then(() => {setCurrent(currentProduct)});
+  }, []);
 
   return (
     <div>
+      {console.log(currentProduct)}
       <h2>Main</h2>
-      <Image images={currentImages} />
+      {/* <Image images={currentImages} /> */}
       <ProductInfo currentProduct={current} currentStyle={currentStyle} />
       <Description />
     </div>
