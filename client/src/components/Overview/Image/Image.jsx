276fd3e961cf4e21f66a24a0e9f257bc22b96e0c
Corey Robinson <cr.contact90@gmail.com>
2021-07-28 14:15:37 -0700

    Created arrow buttons to cycle through photos

--- client/src/components/Overview/Image/Image.jsx
+++ client/src/components/Overview/Image/Image.jsx
@@ -7,20 +7,43 @@ function Image({ images }) {
   const [currentImage, setCurrentImage] = useState({
     active: images[0].url,
     allImages: images,
+    index: 0,
   });
 
-  const changeMainPhoto = (image) => {
-    setCurrentImage({ ...currentImage, active: image });
+  const changeMainPhoto = (image, photoIndex) => {
+    setCurrentImage({ ...currentImage, active: image, index: photoIndex });
+  };
+
+  const nextPhoto = () => {
+    const length = currentImage.allImages.length - 1;
+    const imageIndex = currentImage.index === length ? 0 : currentImage.index + 1;
+    setCurrentImage({
+      ...currentImage,
+      active: currentImage.allImages[imageIndex].thumbnail_url,
+      index: imageIndex,
+    });
+  };
+
+  const prevPhoto = () => {
+    const length = currentImage.allImages.length - 1;
+    const imageIndex = currentImage.index === 0 ? length : currentImage.index - 1;
+    setCurrentImage({
+      ...currentImage,
+      active: currentImage.allImages[imageIndex].thumbnail_url,
+      index: imageIndex,
+    });
   };
 
   return (
     <div className="Image-Component">
+      <i className="fas fa-arrow-left" onClick={() => {prevPhoto(); }} />
+      <i className="fas fa-arrow-right" onClick={() => { nextPhoto(); }} />
       <img className="Main-Image" src={currentImage.active} alt="currentProduct" />
-      {currentImage.allImages.map((image) => (
+      {currentImage.allImages.map((image, index) => (
         <ImageThumbnails
           thumbnail={image.thumbnail_url}
           key={image.url.slice(28)}
-          changeMainPhoto={() => { changeMainPhoto(image.thumbnail_url)}}
+          changeMainPhoto={() => { changeMainPhoto(image.thumbnail_url, index); }}
           currentActive={currentImage.active}
         />
       ))}
