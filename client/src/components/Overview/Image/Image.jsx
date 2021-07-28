17a78d95b38966e7f3c8f484a7d895b5c1e0f66c
Corey Robinson <cr.contact90@gmail.com>
2021-07-28 11:07:09 -0700

    Onclick for changing between image components

--- client/src/components/Overview/Image/Image.jsx
+++ client/src/components/Overview/Image/Image.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+/* eslint-disable import/extensions */
+import React, { useState } from 'react';
+import ImageThumbnails from './ImageThumbnails.jsx';
+import './Image.css';
 
-function Image() {
+function Image({ images }) {
+  const [currentImage, setCurrentImage] = useState({
+    active: images[0].url,
+    allImages: images,
+  });
+
+  const changeMainPhoto = (image) => {
+    setCurrentImage({ ...currentImage, active: image });
+  };
+
   return (
-    <div>
-      <div>Image</div>
+    <div className="Image-Component">
+      <img className="Main-Image" src={currentImage.active} alt="currentProduct" />
+      {currentImage.allImages.map((image) => (
+        <ImageThumbnails
+          thumbnail={image.thumbnail_url}
+          key={image.url.slice(28)}
+          changeMainPhoto={() => { changeMainPhoto(image.thumbnail_url)}}
+          currentActive={currentImage.active}
+        />
+      ))}
     </div>
   );
 }
