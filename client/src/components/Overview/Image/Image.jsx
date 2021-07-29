d748ea88b419725536c19b5e4e9f2ca45ab313c8
Corey Robinson <cr.contact90@gmail.com>
2021-07-28 17:58:44 -0700

    Updated useEffect to track previous index when re-rendering new style, also using thumbnail for all places

--- client/src/components/Overview/Image/Image.jsx
+++ client/src/components/Overview/Image/Image.jsx
@@ -1,18 +1,24 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable import/extensions */
 import React, { useState, useEffect } from 'react';
 import ImageThumbnails from './ImageThumbnails.jsx';
 import './Image.css';
 
 function Image({ images }) {
-
   const [currentImage, setCurrentImage] = useState({
-    active: images[0].url,
+    active: images[0].thumbnail_url,
     allImages: images,
     index: 0,
   });
   //Watches for images prop to change
   useEffect(() => {
-    setCurrentImage({ ...currentImage, allImages: images, index: 0, active: images[0].url});
+    setCurrentImage({
+      ...currentImage,
+      allImages: images,
+      // index: (prevIndex) => {prevIndex; },
+      active: images[currentImage.index].thumbnail_url,
+    });
   }, [images]);
 
   const changeMainPhoto = (image, photoIndex) => {
@@ -41,8 +47,17 @@ function Image({ images }) {
 
   return (
     <div className="Image-Component">
-      <i className="fas fa-arrow-left" onClick={() => {prevPhoto(); }} />
-      <i className="fas fa-arrow-right" onClick={() => { nextPhoto(); }} />
+      <i className="fas fa-arrow-left" />
+      <i className="fas fa-arrow-right" />
+      <i
+        className={currentImage.index === 0 ? 'fa-arrow-up.hidden' : 'fa-arrow-up'}
+        onClick={() => { prevPhoto(); }}
+      />
+      <i
+        className={currentImage.index === currentImage.allImages.length - 1
+          ? 'fa-arrow-down.hidden' : 'fa-arrow-down'}
+        onClick={() => { nextPhoto(); }}
+      />
       <img className="Main-Image" src={currentImage.active} alt="currentProduct" />
       {currentImage.allImages.map((image, index) => (
         <ImageThumbnails
