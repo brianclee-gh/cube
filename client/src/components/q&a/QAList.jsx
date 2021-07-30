beae8cbf613a9eae57ae49100860cd5723343307
Ubin Jung <jungu@utexas.edu>
2021-07-30 15:47:26 -0700

    sorted questions by helpfulness

--- client/src/components/q&a/QAList.jsx
+++ client/src/components/q&a/QAList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable import/extensions */
 /* eslint-disable react/prop-types */
 import React, { useState, useContext, useEffect } from 'react';
@@ -12,35 +13,50 @@ import { QAContext } from '../state/QAContext.jsx';
 // import './qa-style.scss';
 
 const QAList = () => {
-  const { currentProduct } = useContext(ProductsContext);
-  const { getQuestions, questions, getAnswers, answers } = useContext(QAContext);
-//   const [productId, setProductId] = useState(17067);
+  const { currentProduct, getData } = useContext(ProductsContext);
+  const {
+    getQuestions, questions, getAnswers, answers,
+  } = useContext(QAContext);
+  //   const [productId, setProductId] = useState(17067);
   const [data, setData] = useState([]);
 
   const getQAList = async () => {
     if (!currentProduct) { return null; }
     const productId = currentProduct.id;
-    await getQuestions(productId, 1, 5);
-    // console.log(questions);
-    return questions;
+    const fetchedData = await getQuestions(productId, 1, 5);
+    // might not be working.. cannot read property sort of undefined. but i can't find a productId that has questions not sorted in helpfulness already
+    return fetchedData;
   };
 
   useEffect(() => {
-    getQAList()
-      .then()
-      .catch((err) => console.log(err));
-  }, [currentProduct]);
+    getData('17068');
+  }, []);
 
+  useEffect(() => {
+    // const results = await getQAList();
+    // console.log(results);
+    // setData(results.sort((a, b) => ((a.question_helpfulness > b.question_helpfulness) ? -1 : 1)));
+
+    getQAList()
+      .then((fetched) => {
+        if (fetched) {
+          const sorted = fetched.sort((a, b) => ((a.question_helpfulness > b.question_helpfulness) ? -1 : 1));
+          setData(sorted);
+        }
+      })
+      .catch((err) => console.log(err));
+
+    // setData(getQAList().sort((a, b) => ((a.question_helpfulness > b.question_helpfulness) ? -1 : 1)));
+  }, [currentProduct]);
 
   return (
     <div>
       <h2>Questions & Answers</h2>
       <div>
         <QASearch />
-        { questions
-          ? questions.map((q) => <Question question={q} key={q.question_id} />)
-          : 'Loading'
-        }
+        { data
+          ? data.map((q) => <Question question={q} key={q.question_id} />)
+          : 'Loading'}
       </div>
     </div>
   );
