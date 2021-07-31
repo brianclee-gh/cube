9a885d3f54dd607f30c3d2c65d2900e1aa4261dd
Corey Robinson <cr.contact90@gmail.com>
2021-07-30 17:04:30 -0700

    Added default for null

--- client/src/components/Overview/ProductInfo/Styles.jsx
+++ client/src/components/Overview/ProductInfo/Styles.jsx
@@ -4,7 +4,7 @@ function Style({ style, current , isActive, changeActive}) {
   return (
     <>
       <div className="Style-Wrapper" onClick={changeActive}>
-        <img className="Style-Selection" src={style.photos[0].thumbnail_url} alt="" onClick={current} />
+        <img className="Style-Selection" src={style.photos[0].thumbnail_url || 'https://source.unsplash.com/random/600x800'} alt="" onClick={current} />
         {isActive === style ? <div className="checkmark-active far fa-check-circle" /> : null}
       </div>
     </>
