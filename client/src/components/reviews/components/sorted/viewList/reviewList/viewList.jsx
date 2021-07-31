c664dd083505fdd60ce575fc9d89d71bcd4005f0
ehdtlaos <eee910413@gmail.com>
2021-07-31 01:56:51 -0700

    working on body

--- client/src/components/reviews/components/sorted/viewList/reviewList/viewList.jsx
+++ client/src/components/reviews/components/sorted/viewList/reviewList/viewList.jsx
@@ -1,32 +1,85 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import './viewList.css';
 import StarRating from '../../../averageReview/metaRate.jsx';
 import { ReviewsContext } from '../../../../../state/ReviewsContext.jsx';
+import { ProductsContext } from '../../../../../state/ProductsContext.jsx';
 import moment from 'moment';
+import LongerThan from './longerThan.jsx';
 
 function reviewList({sort}) {
   const { reviews } = useContext(ReviewsContext);
+  const { currentProduct } = useContext(ProductsContext);
+  const [viewableReviews, setViewableReviews] = useState(2);
+  const [expandReview, setExpandReview] = useState(null);
+  // const [loadMoreReview, setLoadMoreReview] = useState(true);
 
   const loadMore = () => {
-    expanded ? setDefaultQuestions(4) : setDefaultQuestions(data.length);
-    setExpanded(!expanded);
-  }
+    setViewableReviews(viewableReviews + 2);
+    if (viewableReviews >= reviews.length) {
+      setExpandReview(false);
+    }
+  };
+
+  const showAll = () => {
+    setViewableReviews(reviews.length);
+    setExpandReview(false);
+  };
+
+  // const loadMoreButton = () => {
+  //   setLoadMoreReview(false);
+  // };
+
+  // const allReviewBody = (loads) => {
+  //   return loads
+  // }
+
+  // const moreLoad = (load) => {
+  //   return (
+  //     <div>
+  //       {load}
+  //       <button className="reviewBodyLoadMoreButton" onClick={loadMoreButton}>load more...</button>
+  //     </div>
+  //   )
+  // };
+
+  // const setLongBody = (load) => {
+  //   if (load && loadMoreReview) {
+  //     return true;
+  //   } else if (load && !loadMoreReview) {
+  //     return false;
+  //   }
+  // };
+
+  useEffect(() => {
+    if (reviews !== null) {
+      if (reviews.length <= 2) {
+        setExpandReview(false);
+      } else {
+        setExpandReview(true);
+      }
+    }
+  }, [reviews]);
+
+  useEffect(() => {
+    setViewableReviews(2);
+    setExpandReview(null);
+  }, [currentProduct]);
 
   if (reviews !== null) {
     return (
       <div className="reviewBodyBox">
-        {reviews.map((review) => (
+        {reviews.slice(0, viewableReviews).map((review) => (
           <div className="reviewBodyBox_review" key={review.review_id}>
             <span className="reviewBodyBox_rating">{StarRating(review.rating)}</span>
             <h3 className="reviewBodyBox_title">
-              {review.summary}
+              {review.summary.substring(0, 60)}
             </h3>
             <div className="reviewBodyBox_userCheck" />
             <div className="reviewBodyBox_username">{review.reviewer_name}</div>
             <div className="reviewBodyBox_date">{moment(review.date).format('ll')}</div>
-            <p className="reviewBodyBox_body">
-              {review.body}
-            </p>
+            <div className="reviewBodyBox_body">
+              { (review.body.length >= 250) ? <LongerThan half={review.body.substring(0, 250)} test={review.body}/> : review.body }
+            </div>
             <div className="reviewBodyBox_recommend">
               recommend?
               {String(review.recommend)}
@@ -46,6 +99,8 @@ function reviewList({sort}) {
             </div>
           </div>
         ))}
+        {expandReview ? <button className="reviewShowMoreButton" onClick={loadMore}>More Reviews</button> : null}
+        {expandReview ? <button className="showAllReviewButton" onClick={showAll}>Show All Reviews</button> : null}
       </div>
     );
   }
