15c5ea1d4361afb61e3e8a4b5bc70d0bbd29fc92
Corey Robinson <cr.contact90@gmail.com>
2021-08-04 11:32:50 -0700

    Some Code CleanUp

--- client/src/components/Overview/Image/ImageThumbnails.jsx
+++ client/src/components/Overview/Image/ImageThumbnails.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import React from 'react';
 
 function ImageThumbnails({ thumbnail, changeMainPhoto, currentActive }) {
   return (
     <span className={currentActive === thumbnail ? 'Thumbnail-Wrapper selectActive' : 'Thumbnail-Wrapper selectInactive'}>
-      <img className="Thumbnail-Photo" src={thumbnail} alt="thumbnail" onClick={changeMainPhoto} />
+      <img className="Thumbnail-Photo" src={thumbnail} alt="thumbnail" onClick={changeMainPhoto} onKeyDown={() => {}} />
     </span>
   );
 }
