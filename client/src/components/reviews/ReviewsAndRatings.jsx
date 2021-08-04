89064ae33b7f2a39aaa7379467d8ba5a572e3afd
ehdtlaos <eee910413@gmail.com>
2021-08-04 11:44:43 -0700

    working on css

--- client/src/components/reviews/ReviewsAndRatings.jsx
+++ client/src/components/reviews/ReviewsAndRatings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 import SortList from './components/sorted/sortOptions.jsx';
+import './ReviewsAndRating.css';
 
 function ReviewsAndRatings() {
   return (
